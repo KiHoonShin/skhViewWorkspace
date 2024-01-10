034b2dd1ce5e84ba4588c3f70d24a7a9906406ce
junyoung <153797167+KiHoonShin@users.noreply.github.com>
2024-01-10 18:46:43 +0900

    .

--- Ex12_finalProject/index.js
+++ Ex12_finalProject/index.js
@@ -33,3 +33,15 @@ $('.myslider').slick({
     // instead of a settings object
   ]
 });
+
+$(window).scroll(
+  function () {
+    // 화면의 위쪽을 기준으로 값을 가져오는 애 50보다 크다 => 스크롤을 조금이라도 내렸다
+    if ($(this).scrollTop() > 50) {
+      $('header').addClass('active');
+    } else {
+      // 가장 위인 상태
+      $('header').removeClass('active');
+    }
+  }
+);
