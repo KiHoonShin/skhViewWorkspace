bf6a268305505af392417edab9b6d5bed9929114
KiHoonShin <153797167+KiHoonShin@users.noreply.github.com>
2024-01-11 22:55:02 +0900

    .

--- javascriptEx/ex15arrayFuncQuiz.js
+++ javascriptEx/ex15arrayFuncQuiz.js
@@ -19,32 +19,28 @@ let pets = [cat, dog, rabbit, hamster, cat];
 
 // pets.forEach(obj => console.log(obj));
 
-console.log('1번')
 // 1. 동물의 kind가 개 인것을 찾아라
-// for (let i = 0; i < pets.length; i += 1) {
-//   if (pets[i].kind == "개") {
-//     console.log(pets[i]);
-//   }
-// }
 let res = pets.find((obj) => obj.kind === '개');
-console.log(res);
-console.log('2번')
+console.log('1번 : ',res);
+console.log('------------------')
 // 2. 동물의 kind가 고양이가 아닌것만 빼서 배열로 만들어라
-// for (let i = 0; i < pets.length; i += 1) {
-//   if (pets[i].kind != "고양이") {
-//     console.log(pets[i]);
-//   }
-// }
 let q2 = pets.filter((obj) => obj.kind != "고양이");
-console.log(q2);
+console.log('2번 : ',q2);
 
-
-console.log('3번')
+console.log('------------------')
 // 3. 총 동물의 평균 나이를 구해라
+let q3 = pets.reduce((p , n)=> {
+return p + n.age ;
+},0);
+console.log('3번 : ' , q3 / pets.length);
 
-console.log('4번')
+console.log('------------------')
+
 // 4. 동물 나이순으로 내림차순 정렬
 
+console.log('4번')
+let copy = [...pets];
+copy.sort((a,b)=> a.age > b.age ? -1 : 1);
+console.log(copy);
 
-// let result = pets.age.reduce((p, n) => p + n, 0);
-// console.log(result);
+
